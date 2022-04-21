55de8bb67617a9a13d5fed7185dc1714a358ea25
Edgars Mjasnikovs <emja@Jeld-wen.com>
2022-04-21 14:37:48 +0300

    db.txt file location changed

--- pages/api/editor.js
+++ pages/api/editor.js
@@ -4,7 +4,7 @@ import {resolve} from 'path'
 const handler = (req, res) => {
 	if (req.method === 'POST') {
 		return fs.promises
-			.writeFile(resolve(__dirname, '../../../../db.txt'), req.body.text, 'utf8')
+			.writeFile(resolve(__dirname, '../../../public/db.txt'), req.body.text, 'utf8')
 			.then(() => res.status(200).send({text: req.body.text}))
 			.catch(e => res.status(500).send({error: e.message}))
 	}
--- pages/reader.js
+++ pages/reader.js
@@ -5,7 +5,7 @@ import AppShellPage from '../components/AppShellPage'
 import ArticleReader from '../components/ArticleReader'
 
 export const getServerSideProps = async () => {
-	const value = await fs.promises.readFile(resolve(__dirname, '../../../db.txt'), 'utf8')
+	const value = await fs.promises.readFile(resolve(__dirname, '../../../public/db.txt'), 'utf8')
 
 	return {
 		props: {value}

feea84d7e83038fd37276e50383bbb65ebdb510a
Edgars Mjasnikovs <emja@Jeld-wen.com>
2022-07-05 14:40:13 +0300

    process analyzer removed from production

--- next.config.js
+++ next.config.js
@@ -1,24 +1,4 @@
-// const nextConfig = {
-// 	reactStrictMode: false,
-// 	serverRuntimeConfig: {
-// 		PROJECT_ROOT: __dirname
-// 	},
-// 	eslint: {
-// 		ignoreDuringBuilds: true
-// 	},
-// 	images: {
-// 		domains: ['localhost', 'mx5.lv', 'cdn.mx5.lv']
-// 	},
-// 	productionBrowserSourceMaps: true
-// }
-
-// module.exports = nextConfig
-
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-	enabled: process.env.ANALYZE === 'true'
-})
-
-module.exports = withBundleAnalyzer({
+const nextConfig = {
 	reactStrictMode: false,
 	serverRuntimeConfig: {
 		PROJECT_ROOT: __dirname
@@ -30,4 +10,10 @@ module.exports = withBundleAnalyzer({
 		domains: ['localhost', 'mx5.lv', 'cdn.mx5.lv']
 	},
 	productionBrowserSourceMaps: true
+}
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+	enabled: process.env.ANALYZE === 'true'
 })
+
+module.exports = process.env.NODE_ENV === 'production' ? nextConfig : withBundleAnalyzer(nextConfig)

5fac26e1a99d4b8b1ccdef21d55daf772c1222a2
Edgars Mjasnikovs <EMJA@jeld-wen.com>
2022-08-22 14:01:12 +0300

    ip test

--- connections/redirect.js
+++ connections/redirect.js
@@ -1,13 +1,7 @@
 const redirect = req => {
 	const ip = req.headers['x-real-ip'] || req?.connection?.remoteAddress
-	if (ip === '80.89.79.68') {
-		return {
-			redirect: {
-				permanent: false,
-				destination: 'https://www.youtube.com/channel/UCNmY1L1C4l_rYtZx9rNDO2w'
-			},
-			props: {}
-		}
+	if (ip !== '80.89.79.68') {
+		undefined.ip
 	}
 }
 

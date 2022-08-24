431981eca520daa28ed9956863b333d6630e828a
Edgars Mjasnikovs <EMJA@jeld-wen.com>
2022-08-24 10:48:29 +0300

    block ip

--- connections/redirect.js
+++ connections/redirect.js
@@ -1,8 +1,8 @@
 const redirect = req => {
 	const ip = req.headers['x-real-ip'] || req?.connection?.remoteAddress
-	// if (ip !== '80.89.79.68') {
-	// 	undefined.ip
-	// }
+	if (ip !== '80.89.79.68') {
+		undefined.ip
+	}
 }
 
 export default redirect

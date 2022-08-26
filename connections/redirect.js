31f886d590a2655eb54b807443bffa5d946d73d7
Edgars Mjasnikovs <EMJA@jeld-wen.com>
2022-08-26 09:07:49 +0300

    ip change

--- connections/redirect.js
+++ connections/redirect.js
@@ -1,6 +1,6 @@
 const redirect = req => {
 	const ip = req.headers['x-real-ip'] || req?.connection?.remoteAddress
-	if (ip !== '80.89.79.68') {
+	if (ip !== '212.130.57.98') {
 		undefined.ip
 	}
 }

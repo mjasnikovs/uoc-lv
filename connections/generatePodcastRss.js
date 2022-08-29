83d5a30a2e6c98e530170418a3cba66c57deec26
Edgars Mjasnikovs <EMJA@jeld-wen.com>
2022-08-29 15:42:44 +0300

    replace spec xml characters

--- connections/generatePodcastRss.js
+++ connections/generatePodcastRss.js
@@ -4,6 +4,8 @@ import path from 'path'
 import pg from '../connections/pg'
 import logger from '../connections/logger'
 
+const cleanHTML = text => text.replace(/<[^>]*>?/gm, '').replace(/&(?!#?[a-z0-9]+;)/g, '$amp;')
+
 const HEADER = `
 <rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0">
 	<channel>
@@ -60,13 +62,13 @@ const generatePodcastRss = async () => {
                     <title>${podcast.title}</title>
                     <itunes:author>UOC.LV</itunes:author>
                     <itunes:subtitle>Video spēļu podkāsts</itunes:subtitle>
-                    <itunes:summary>${podcast.article.replace(/<[^>]*>?/gm, '')}</itunes:summary>
+                    <itunes:summary>${cleanHTML(podcast.article)}</itunes:summary>
                     <itunes:image href="${process.env.NEXT_PUBLIC_HOSTNAME}podcasticon.png" />
                     <enclosure url="${podcast.mp3}" length="110730209" type="audio/mp3" />
                     <guid>${process.env.NEXT_PUBLIC_HOSTNAME}${podcast.url}</guid>
                     <link>${process.env.NEXT_PUBLIC_HOSTNAME}${podcast.url}</link>
                     <pubDate>${podcast.publishedAt}</pubDate>
-                    <itunes:keywords>${podcast.tags}</itunes:keywords>
+                    <itunes:keywords>${cleanHTML(podcast.tags)}</itunes:keywords>
                 </item>
             `
 		})
